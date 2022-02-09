55938959cdb3a1618853d111dcf2ac86f5df441c
Alejandro Mantilla <alejandromantillaredondo@gmail.com>
2022-02-09 11:49:56 -0500

    Added comments to code

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Create Flask service.
 app = Flask(__name__)
 
+# Add local database to app config.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+# Remove unused sqlalchemy event system. Defaul is None, which is falsy.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create database object using app config dictionary
+db = SQLAlchemy(app)
+
+# Main execute
 if __name__ == '__main__':
-	app.run(debug = True)
-    
+
+    app.run(debug=True)
