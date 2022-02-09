763b039f800be0f0c2436f5d23d81844e32125ba
Alejandro Mantilla <alejandromantillaredondo@gmail.com>
2022-02-09 12:24:27 -0500

    Added class for 'Publicación' table.

--- app.py
+++ app.py
@@ -13,7 +13,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Create database object using app config dictionary
 db = SQLAlchemy(app)
 
+# Class for template of table: publicacion. Lista de publicaciones con sus atributos
+class Publicacion(db.Model):
+
+	# Columna identidad e índice (sin valores repetidos)
+    id = db.Column(db.Integer, primary_key = True)
+
+	# Columna con el título de cada publicación.
+    titulo = db.Column( db.String(50) )
+
+	# Columna con el contenido de la publicación 
+    contenido = db.Column( db.String(255) )
+
 # Main execute
 if __name__ == '__main__':
 
+	# Desplegar el servicio
     app.run(debug=True)
